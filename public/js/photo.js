afa38d6a5fb494d4af6965990351a8e39fbd314d
Dave Schaaf <apprentice@dbc09.local>
2014-05-06 20:37:17 -0700

    image markers on photo page

--- public/js/photo.js
+++ public/js/photo.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     // google.maps.event.addDomListener(window, 'load', setBackgroundImage);
     google.maps.event.addDomListener(window, 'load', initializeGoogleMap);
     // google.maps.event.addListener(marker, 'click', toggleBounce)
+
 });
 
 
@@ -20,21 +21,22 @@ function initializeGoogleMap() {
 
     var map = new google.maps.Map(document.getElementById("map-canvas"), {
         center: mapCenter,
-        zoom: 6
+        zoom: 6,
+        mapTypeId: google.maps.MapTypeId.HYBRID
     });
 
-    // var image = {
-    //     url: document.getElementById('my-photo').querySelector('img').src,
-    //     size: new google.maps.Size(384, 256),
-    //     origin: new google.maps.Point(0, 0),
-    //     anchor: new google.maps.Point(0, 256)
-    // }
+    var featuredPicture = document.getElementById('my-photo').querySelector('img')
+
+    var image = {
+        url: featuredPicture.src,
+        scaledSize: new google.maps.Size(featuredPicture.width * .1, featuredPicture.height * .1)
+    }
 
     var marker = new google.maps.Marker({
         position: markerPoint,
         map: map,
         title: 'Hampi',
-        // icon: image,
+        icon: image,
         animation: google.maps.Animation.DROP
     });
 }
@@ -48,3 +50,35 @@ function initializeGoogleMap() {
 //         marker.setAnimation(google.maps.Animation.BOUNCE);
 //     }
 // }
+
+
+var latLng = new google.maps.LatLng(49.47805, -123.84716);
+var homeLatLng = new google.maps.LatLng(49.47805, -123.84716);
+
+var map = new google.maps.Map(document.getElementById('map_canvas'), {
+    zoom: 12,
+    center: latLng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+var pictureLabel = document.createElement("img");
+pictureLabel.src = "home.jpg";
+
+var marker = new MarkerWithLabel({
+    position: homeLatLng,
+    map: map,
+    draggable: true,
+    labelContent: pictureLabel,
+    labelAnchor: new google.maps.Point(50, 0),
+    labelClass: "labels", // the CSS class for the label
+    labelStyle: {
+        opacity: 0.50
+    }
+});
+
+var iw = new google.maps.InfoWindow({
+    content: "Home For Sale"
+});
+google.maps.event.addListener(marker, "click", function(e) {
+    iw.open(map, marker);
+});
