78f235f25e6f18ccb045da5e42d999f8aedad580
Dave Schaaf <apprentice@dbc09.local>
2014-05-08 13:52:50 -0700

    Collage, albums and photos views working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,12 +12,6 @@ get '/photos' do
   erb :photo
 end
 
-get '/photos/:id' do
-  @photo = Photo.find(params[:id])
-  erb :photo
-end
-
-
 get '/path' do
   erb :path
 end
@@ -32,13 +26,17 @@ post '/photos/new' do
   redirect '/photos/#{@id}'
 end
 
+get '/photos/:id' do
+  @photo = Photo.find(params[:id])
+  erb :photo
+end
 
 get '/albums' do
-
-  erb :album
+  erb :all_albums
 end
 
 get '/albums/all' do
+
   @albums = Album.all
 
   transfer_data = @albums.map do |album|
@@ -57,8 +55,11 @@ get '/albums/all' do
   Hash[transfer_data].to_json
 end
 
-
 get '/albums/:id' do
+  erb :album
+end
+
+get '/albums/:id/photos' do
 
   @album = Album.find(params[:id])
 
