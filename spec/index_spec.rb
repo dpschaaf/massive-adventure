8acad036b6006da190b62cd4db1ea2d82b3260ba
Drew West & James Hulley <pair+drewfwest.jhulley@devbootcamp.com>
2014-05-08 11:29:13 -0700

    Added /photos route testing

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -6,14 +6,36 @@ require 'spec_helper'
 
 describe '/' do
 
-  it "return erb :index when '/' route is hit" do
+  it "return an html file when '/' route is hit" do
     # No arrange
 
     get '/'
 
     expect(last_response["Content-Type"]).to eq("text/html;charset=utf-8")
   end
+end
 
-  it '/photos' do
+describe '/photos' do
+
+  it "return an html file when '/' route is hit" do
+    Photo.destroy_all
+    10.times { Photo.create() }
+
+    get '/photos'
+
+    expect(last_response["Content-Type"]).to eq("text/html;charset=utf-8")
+  end
+
+  it "have a photo url in the html file" do
+    Photo.destroy_all
+    test_url = "happy_fun_time"
+    Photo.create(url: test_url)
+
+    get '/photos'
+
+    expect(last_response.body).to include(test_url)
+  end
 
 end
+
+describe '/'
